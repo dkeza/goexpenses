f5236f60fd7f0aa2e84334d5e4ecbb0b73c057c8
dkeza <dkezic@yahoo.com>
2018-10-14 10:30:36 +0200

    Add more tables

--- db/pg_structure.sql
+++ db/pg_structure.sql
@@ -35,7 +35,7 @@ DROP TABLE IF EXISTS public.sessions;
 CREATE TABLE public.sessions
 (
   id SERIAL PRIMARY KEY,
-  uuid character varying unique NOT NULL DEFAULT ''::character varying,
+  uuid character varying UNIQUE NOT NULL DEFAULT ''::character varying,
   user_id integer NOT NULL DEFAULT 0,
   lang character varying NOT NULL DEFAULT 'EN'::character varying,
   message character varying NOT NULL DEFAULT ''::character varying,
@@ -61,5 +61,81 @@ CREATE TABLE public.posts
   deleted boolean NOT NULL DEFAULT false,
   p_id character varying NOT NULL DEFAULT ''::character varying
   );
-  
-  
+
+-- Table: public.currencies
+
+DROP TABLE IF EXISTS public.currencies;
+
+CREATE TABLE public.currencies
+(
+  id SERIAL PRIMARY KEY,
+  code character varying UNIQUE NOT NULL DEFAULT ''::character varying,
+  rate decimal(12,4) NOT NULL DEFAULT 0,
+  date date
+);
+
+-- Table: public.expenses
+
+DROP TABLE IF EXISTS public.expenses;
+
+CREATE TABLE public.expenses
+(
+  id SERIAL PRIMARY KEY,
+  description character varying NOT NULL DEFAULT ''::character varying,
+  accounts_id integer NOT NULL DEFAULT 0,
+  amount decimal(12,2) NOT NULL DEFAULT 0,
+  exchange decimal(12,4) NOT NULL DEFAULT 0,
+  deleted boolean NOT NULL DEFAULT false,
+  expenses_id integer NOT NULL DEFAULT 0,
+  p_id character varying NOT NULL DEFAULT ''::character varying
+);
+
+-- Table: public.incomes
+
+DROP TABLE IF EXISTS public.incomes;
+
+CREATE TABLE public.incomes
+(
+  id SERIAL PRIMARY KEY,
+  description character varying NOT NULL DEFAULT ''::character varying,
+  accounts_id integer NOT NULL DEFAULT 0,
+  deleted boolean NOT NULL DEFAULT false,
+  p_id character varying NOT NULL DEFAULT ''::character varying
+);
+
+-- Table: public.params
+
+DROP TABLE IF EXISTS public.params;
+
+CREATE TABLE public.params
+(
+  id SERIAL PRIMARY KEY,
+  build integer NOT NULL DEFAULT 0
+);
+
+
+-- Table: public.passwordresets
+
+DROP TABLE IF EXISTS public.passwordresets;
+
+CREATE TABLE public.passwordresets
+(
+  id SERIAL PRIMARY KEY,
+  email character varying NOT NULL DEFAULT ''::character varying,
+  token character varying UNIQUE NOT NULL DEFAULT ''::character varying,
+  created_at timestamp NOT NULL DEFAULT NOW(),
+  done boolean NOT NULL DEFAULT false
+  );
+
+-- Table: public.accountsusers
+
+DROP TABLE IF EXISTS public.accountsusers;
+
+CREATE TABLE public.accountsusers
+(
+  id SERIAL PRIMARY KEY,
+  accounts_id integer NOT NULL DEFAULT 0,
+  users_id integer NOT NULL DEFAULT 0
+);
+
+
