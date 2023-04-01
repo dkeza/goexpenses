c2c5a65e57a278844b8238d282bdf0d98df8ce4d
keza <dkezic@yahoo.com>
2023-04-01 22:50:17 +0200

    Version v10

--- util/settings.go
+++ util/settings.go
@@ -29,7 +29,7 @@ type AppSettings struct {
 func ReadSettings() {
 
 	// Here set build version for every release
-	Settings.Build = 9
+	Settings.Build = 10
 
 	Settings.Host = ""
 	Settings.Port = ""
